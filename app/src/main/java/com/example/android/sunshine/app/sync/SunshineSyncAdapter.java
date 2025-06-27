a2e94332ea07e41ad2a8a283b94d68444e1e56cc
Anthony <aenutter@live.com>
2025-06-27 11:47:34 -0500

    removed two pane

--- app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
@@ -88,7 +88,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
 
         String format = "json";
         String units = "imperial";
-        int numDays = 1;
+        int numDays = 14;
 
         try {
             // Construct the URL for the OpenWeatherMap query
