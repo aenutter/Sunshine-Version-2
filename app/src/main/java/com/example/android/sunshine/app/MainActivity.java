e320998370dde4a3f767c5bfe854418b20bea0c5
Anthony <aenutter@live.com>
2025-08-10 11:48:58 -0500

    sql update at midnight

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -15,14 +15,25 @@
  */
 package com.example.android.sunshine.app;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.android.sunshine.app.data.WeatherContract;
+import com.example.android.sunshine.app.data.WeatherDbHelper;
 import com.example.android.sunshine.app.sync.DogSyncAdapter;
+
+import java.util.Calendar;
 
 public class MainActivity extends ActionBarActivity implements DogFragment.Callback {
 
@@ -77,6 +88,7 @@ public class MainActivity extends ActionBarActivity implements DogFragment.Callb
         dogFragment.setUseTodayLayout(!mTwoPane);
 
         DogSyncAdapter.initializeSyncAdapter(this);
+        createAlarm();
     }
 
     @Override
@@ -141,6 +153,60 @@ public class MainActivity extends ActionBarActivity implements DogFragment.Callb
             Intent intent = new Intent(this, DetailActivity.class)
                     .setData(contentUri);
             startActivity(intent);
+        }
+    }
+    public void createAlarm() {
+        //System request code
+        int DATA_FETCHER_RC = 123;
+        //Create an alarm manager
+        AlarmManager mAlarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+
+        //Create the time of day you would like it to go off. Use a calendar
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+
+        //Create an intent that points to the receiver. The system will notify the app about the current time, and send a broadcast to the app
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, DATA_FETCHER_RC,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //initialize the alarm by using inexactrepeating. This allows the system to scheduler your alarm at the most efficient time around your
+        //set time, it is usually a few seconds off your requested time.
+        // you can also use setExact however this is not recommended. Use this only if it must be done then.
+
+        //Also set the interval using the AlarmManager constants
+        mAlarmManager.setInexactRepeating(AlarmManager.RTC,calendar.getTimeInMillis(),AlarmManager.INTERVAL_DAY, pendingIntent);
+
+    }
+    //This is the broadcast receiver you create where you place your logic once the alarm is run. Once the system realizes your alarm should be run, it will communicate to your app via the BroadcastReceiver. You must implement onReceive.
+    public static class AlarmReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Your code once the alarm is set off goes here
+            //You can use an intent filter to filter the specified intent
+            WeatherDbHelper mOpenHelper;
+            mOpenHelper = new WeatherDbHelper(context);
+            final SQLiteDatabase database = mOpenHelper.getWritableDatabase();
+//            String selection = WeatherContract.DogEntry.TABLE_NAME+
+//                    "." + WeatherContract.DogEntry.COLUMN_ID + " = ? ";
+//            String[] selectionArgs = new String[]{String.valueOf(itemID)};
+
+            ContentValues values = new ContentValues();
+            values.put(WeatherContract.DogEntry.COLUMN_DOG_PLAYGROUP, 0);
+            values.put(WeatherContract.DogEntry.COLUMN_DOG_WALK_AM, 0);
+            values.put(WeatherContract.DogEntry.COLUMN_DOG_WALK_PM, 0);
+            values.put(WeatherContract.DogEntry.COLUMN_DOG_OFFICE, 0);
+            values.put(WeatherContract.DogEntry.COLUMN_DOG_VISITOR, 0);
+            values.put(WeatherContract.DogEntry.COLUMN_DOG_VOLUNTEER_ROOM, 0);
+            values.put(WeatherContract.DogEntry.COLUMN_DOG_ADVENTURE_TAILS, 0);
+            // Add more columns and their new values as needed
+
+            Integer rowsUpdated = database.update(WeatherContract.DogEntry.TABLE_NAME, values, null, null);
+//            Integer rowsDeleted = database.delete(WeatherContract.DogEntry.TABLE_NAME, selection, selectionArgs);
+            Toast.makeText(context, "Action Delete rows updated: " + rowsUpdated, Toast.LENGTH_LONG).show();
+            MyLogger.d("sunshine", "inside AlarmReceiver rows updated: " + rowsUpdated);
+            database.close();
+            context.getContentResolver().notifyChange(WeatherContract.DogEntry.CONTENT_URI, null);
         }
     }
 }
